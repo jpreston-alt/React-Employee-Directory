1a55eee33fca07a83813740b3338099ab078e1b0
Joanna Preston <joannappreston@gmail.com>
2020-07-14 13:24:11 -0700

    Updates

---
+++ precache-manifest.ccb2f0f3f8a3d8b1e28a506df9f5ef59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8a7444b81ed4f377a752a37b8746ce",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "16e11bd999f83ba2cde6",
+    "url": "/React-Employee-Directory/static/css/main.6bce7e52.chunk.css"
+  },
+  {
+    "revision": "791e6c6efe4fb282968d",
+    "url": "/React-Employee-Directory/static/js/2.d90ddee7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.d90ddee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e11bd999f83ba2cde6",
+    "url": "/React-Employee-Directory/static/js/main.c473dec1.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
