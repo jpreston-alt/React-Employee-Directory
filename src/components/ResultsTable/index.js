5a5ba59a260ae89f7c12819b0ef694b54b04cccd
Joanna Preston <joannappreston@gmail.com>
2020-07-14 13:21:49 -0700

    commit before deploy

--- src/components/ResultsTable/index.js
+++ src/components/ResultsTable/index.js
@@ -3,43 +3,55 @@ import "./style.css";
 import EmployeeRow from "./EmployeeRow";
 import TableHead from "./TableHead";
 
-function ResultsTable(props) {
-    function formatPhoneNumber(phoneNumberString) {
-        var cleaned = ('' + phoneNumberString).replace(/\D/g, '')
-        var match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/)
+class ResultsTable extends React.Component {
+
+    formatPhoneNumber(phoneNum) {
+        // strips the string of all characeters that aren't a number
+        var numStripped = ('' + phoneNum).replace(/\D/g, '')
+
+        // retrieves the parts of the string that match the parts of a formatted phone number
+        var match = numStripped.match(/^(\d{3})(\d{3})(\d{4})$/);
+
+        // if matches exist return a formatted phone number
         if (match) {
             return '(' + match[1] + ') ' + match[2] + '-' + match[3]
         }
-        return phoneNumberString;
+
+        // if number didn't match regularly formatted number, return original number
+        return phoneNum;
     };
 
-    return (
-        <div className="uk-container table-container">
-            <table className="uk-table uk-table-striped uk-table-responsive table">
-                <TableHead
-                    handleArrowClick={props.handleArrowClick}
-                    order={props.order}
-                />
-                <tbody>
-                    {
-                        props.employees.map(e => {
-                            let date = new Date(e.dob.date);
-                            let dateStr = date.toLocaleDateString();
+    // console.log(window.innerWidth);
 
-                            return <EmployeeRow
-                                key={props.employees.indexOf(e)}
-                                img={e.picture.thumbnail}
-                                name={`${e.name.first} ${e.name.last}`}
-                                phone={formatPhoneNumber(e.phone)}
-                                email={e.email}
-                                dob={dateStr}
-                            />
-                        })
-                    }
-                </tbody>
-            </table>
-        </div>
-    )
+    render() {
+        return (
+            <div className="uk-container table-container">
+                <table className="uk-table uk-table-striped uk-table-responsive table">
+                    <TableHead
+                        handleArrowClick={this.props.handleArrowClick}
+                        order={this.props.order}
+                    />
+                    <tbody>
+                        {
+                            this.props.employees.map(e => {
+                                let date = new Date(e.dob.date);
+                                let dateStr = date.toLocaleDateString();
+
+                                return <EmployeeRow
+                                    key={this.props.employees.indexOf(e)}
+                                    img={e.picture.thumbnail}
+                                    name={`${e.name.first} ${e.name.last}`}
+                                    phone={this.formatPhoneNumber(e.phone)}
+                                    email={e.email}
+                                    dob={dateStr}
+                                />
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
 };
 
 export default ResultsTable;
